fdaa72a1205709036f7e4d28a4ce8b6bcc4d32dc
EuverteT <thomaseuverte@gmail.com>
2024-07-17 06:03:46 +0200

    Composant LOCATION / Récup Id dans la route / Modif ID dans logements.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LocationListComponent } from './location-list/location-list.component';
+import { LocationComponent } from './location/location.component';
 
 export const routes: Routes = [
     { path: '', component: LocationListComponent },
-    { path: 'about', component: AboutComponent}
+    { path: 'about', component: AboutComponent},
+    { path: 'location/:id', component: LocationComponent}
 ];
---
+++ src/app/location/location.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { LocationListComponent } from '../location-list/location-list.component';
+import { LocationService } from '../location.service';
+
+
+@Component({
+  selector: 'app-location',
+  standalone: true,
+  imports: [RouterOutlet, LocationListComponent],
+  templateUrl: './location.component.html',
+  styleUrl: './location.component.scss'
+})
+export class LocationComponent implements OnInit{
+
+  private locationService = inject(LocationService);
+  locations = this.locationService.locations;
+
+  id = 0
+  private route = inject(ActivatedRoute);
+
+  ngOnInit() {
+      this.id = +this.route.snapshot.paramMap.get('id')!;
+
+    };
+  
+
+
+}
