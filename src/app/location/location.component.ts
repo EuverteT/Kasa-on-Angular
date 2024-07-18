45efcd46b55a835442203c3a4df668566faf3d2f
EuverteT <thomaseuverte@gmail.com>
2024-07-18 19:16:40 +0200

    Page Location: slider photos

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { LocationListComponent } from '../location-list/location-list.component';
 import { LocationService } from '../location.service';
@@ -11,19 +11,42 @@ import { LocationService } from '../location.service';
   templateUrl: './location.component.html',
   styleUrl: './location.component.scss'
 })
-export class LocationComponent implements OnInit{
+export class LocationComponent implements OnInit {
 
   private locationService = inject(LocationService);
   locations = this.locationService.locations;
-  id = 0
   private route = inject(ActivatedRoute);
+  
+  id = 0
+  pictureIndex = 0
 
   ngOnInit() {
-      this.id = +this.route.snapshot.paramMap.get('id')!;
+    this.id = +this.route.snapshot.paramMap.get('id')!;
 
-      this.locationService.getLocations().subscribe();
+    this.locationService.getLocations().subscribe();
 
-    };
-  
-  
+    this.pictureIndex = 0;
+
+  };
+
+  prev() {
+    let arrayLength = this.locations()[this.id].pictures.length
+    if (this.pictureIndex == 0) {
+      this.pictureIndex = arrayLength;
+    }
+    if ((this.pictureIndex > 0) && (this.pictureIndex != 0)) {
+      this.pictureIndex = this.pictureIndex - 1;
+    }
+  }
+
+  next() {
+    let arrayLength = this.locations()[this.id].pictures.length
+    if (this.pictureIndex < (arrayLength)) {
+      this.pictureIndex = this.pictureIndex + 1;
+    }
+    if (this.pictureIndex == (arrayLength)) {
+      this.pictureIndex = 0;
+    }
+  }
+
 }
